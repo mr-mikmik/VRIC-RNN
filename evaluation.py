cebff61bc884a9bcc1552df4c2bbb288e774823a
EC2 Default User <ec2-user@ip-172-31-32-192.us-west-2.compute.internal>
2017-12-03 12:24:21 +0000

    debugging

--- evaluation.py
+++ evaluation.py
@@ -5,7 +5,7 @@ import torchvision.transforms as transforms
 from torch.autograd import Variable
 
 import os
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import scipy.misc
 import numpy as np
 
@@ -26,8 +26,8 @@ PRINT_EVERY = 2000
 SAVE_STEP = 5000
 
 MODEL_PATH = './saved_models/'
-ENCODER_PATH = './saved_models/.plk'
-DECODER_PATH = './saved_models/.plk'
+ENCODER_PATH = './saved_models/encoder-3-12500.pkl'
+DECODER_PATH = './saved_models/decoder-3-12500.pkl'
 
 ###########################
 
@@ -49,6 +49,7 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
 
 dataiter = iter(testloader)
 
+print('Loading Models')
 # Initialize the models
 encoder = models.EncoderFC(CODED_SIZE, PATCH_SIZE)
 decoder = models.DecoderFC(CODED_SIZE, PATCH_SIZE)
@@ -57,9 +58,10 @@ decoder = models.DecoderFC(CODED_SIZE, PATCH_SIZE)
 encoder.load_state_dict(torch.load(ENCODER_PATH))
 decoder.load_state_dict(torch.load(DECODER_PATH))
 
+print('Starting eval:::::::::::::::::')
 for i in range(5):
     imgs, _ = dataiter.next()
     imsave(torchvision.utils.make_grid(imgs), 'prova1.jpg')
-    feats = encoder(imgs)
+    feats = encoder(Variable(imgs))
     outputs = decoder(feats)
-    imsave(torchvision.utils.make_grid(outputs), 'prova1_decoded.jpg')
+    imsave(torchvision.utils.make_grid(torch.Tensor(outputs.data)), 'prova1_decoded.jpg')
