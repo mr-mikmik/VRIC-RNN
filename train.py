286d016acfd3c458c4c3881787be444c473a60e3
mr-mikmik <aglaner@hotmail.com>
2017-12-10 11:03:53 +0100

    adding changes lstm

--- train.py
+++ train.py
@@ -62,7 +62,7 @@ def main(args):
             for patch in patches:
                 # Transform the tensor into Variable
                 v_patch = Variable(patch)
-                target_tensor = Variable(torch.zeros(v_patch.size()).double(), requires_grad=False)
+                target_tensor = Variable(torch.zeros(v_patch.size()), requires_grad=False)
                 losses = []
                 # Set gradients to Zero
                 optimizer.zero_grad()
